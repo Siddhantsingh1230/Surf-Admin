474f21d365701a664a92135ab9dc3e343b498a56
khushi kandoi <khushikandoii09@gmail.com>
2023-12-14 12:32:32 +0530

    changed order slice

--- src/api/Orders_api.js
+++ src/api/Orders_api.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
 export const getAllOrders = async (page) => {
-    // changed from http://localhost:8080/orders?_page=${page}&_limit=10
-    const response = await axios.get(`http://localhost:8080/orders/all?_page=${page}&_limit=10`);
-    const totalItems = response.headers["x-total-count"];
-    return {data:response.data,totalItems:totalItems};
+  // changed from http://localhost:8080/orders?_page=${page}&_limit=10
+  const response = await axios.get(
+    `http://localhost:8080/orders?_page=${page}&_limit=10`
+  );
+  const totalItems = response.headers["x-total-count"];
+  return { data: response.data, totalItems: totalItems };
 };
 
 export const updateOrder = async (order) => {
-    const {data} = await axios.patch("http://localhost:8080/orders/"+order.id,{
-        ...order
-    });
-    return order;
+  const { data } = await axios.patch(
+    "http://localhost:8080/orders/" + order.id,
+    {
+      ...order,
+    }
+  );
+  return order;
 };
